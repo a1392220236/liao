016de12b6ff0653c2184f12482b8f20542bbd415
liaoyanghui <1392220236@qq.com>
2018-11-12 14:40:45 +0800

    123

---
+++ application/manage/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Yirius
+ * Date: 2018/6/25
+ * Time: 14:35
+ */
+
+namespace app\manage\controller;
+
+use icesadmin\extend\manage\Manage;
+
+class Index extends Manage
+{
+
+}
---
+++ config/captcha.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: Yirius
+ * Date: 2018/6/27
+ * Time: 22:52
+ */
+return [
+    'expire' => 180,
+    'useZh' => false,
+    'useImgBg' => false,
+    'fontSize' => 35,
+    'useCurve' => false,
+    'useNoise' => true,
+    'imageH' => 0,
+    'imageW' => 0,
+    'length' => 3,
+    'reset' => true
+];
