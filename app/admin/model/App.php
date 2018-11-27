20f650ef66e041d4636e1acdfd0544452b6a3282
liaoyanghui <1392220236@qq.com>
2018-11-27 14:39:58 +0800

    更新应用列表

--- app/admin/model/App.php
+++ app/admin/model/App.php
@@ -11,6 +11,28 @@ namespace app\admin\model;
 use \think\Model;
 class App extends Model
 {
+    public function admincate()
+    {
+        //关联角色表
+        return $this->belongsTo('AdminCate');
+    }
 
+    public function article()
+    {
+        //关联文章表
+        return $this->hasOne('Article');
+    }
+
+    public function log()
+    {
+        //关联日志表
+        return $this->hasOne('AdminLog');
+    }
+
+    public function attachment()
+    {
+        //关联附件表
+        return $this->hasOne('Attachment');
+    }
 
 }
