e92e374edd6f221f590b3d73a813b2e19ec2f3b8
liaoyanghui <1392220236@qq.com>
2018-11-27 11:11:34 +0800

    应用列表

---
+++ app/admin/controller/App.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/27
+ * Time: 11:09
+ */
+
+namespace app\admin\controller;
+
+
+class App
+{
+
+}
---
+++ app/admin/model/App.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/27
+ * Time: 11:11
+ */
+
+namespace app\admin\model;
+
+
+class App
+{
+
+}
